c72fc755ffbcc8eb0ef8e7c5493bbd47fe6be2b6
Rose Kairu <rosekairu@gmail.com>
2020-06-13 19:57:34 +0300

    modified config file

--- config.py
+++ config.py
@@ -8,22 +8,24 @@ class Config:
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rose:kairu@localhost/kairublog'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     
 
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+   
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
+    SUBJECT_PREFIX = 'KairuBlog'
+    SENDER_EMAIL = 'moringawatchlist@gmail.com'
+
     # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    SUBJECT_PREFIX = 'kairublog'
-    SENDER_EMAIL = 'moringawatchlist@gmail.com'
+    
 
     @staticmethod
     def init_app(app):
@@ -37,13 +39,11 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
-
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://rose:kairu@localhost/kairublog'
     DEBUG = True
 
-
-    
+  
 config_options = {
 'development':DevConfig,
 'production':ProdConfig,
