d2ce2b47bc879cefdefa0f02229031deb3ec40ec
Rose Kairu <rosekairu@gmail.com>
2020-06-15 16:11:29 +0300

    working on tests

--- tests/test_blog.py
+++ tests/test_blog.py
@@ -2,27 +2,19 @@ import unittest
 from app.models import Blog, User, Comment
 from app import db
 
-class TestBlog(unittest.TestCase):
+class BlogModelTest(unittest.TestCase):
     def setUp(self):
         self.user_Rose = User(username = 'Rose', email = 'rose@ms.com', password = 'cairo')
         self.new_blog = Blog(title='Title for the blog',description = "pineapples")
     
     def tearDown(self):
-        Blog.session.delete(self)
+        Blog.query.delete(self)
         User.query.delete()
-        
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_blog,Blog))
-
+    
     def test_check_instance_variables(self):
         self.assertEquals(self.new_blog.description,"pineapples")
-        self.assertEquals(self.new_blog.title,'Title for the blog')
+        self.assertEquals(self.new_blog.title,'Title')
 
     def test_save_blog(self):
-        self.new_comment.save_blog()
+        self.new_blog.save_blog()
         self.assertTrue(len(Blog.query.all())>0)
-
-    def test_get_blogs_by_id(self):
-        self.new_blog.save_comment()
-        got_blogs = Blog.get_blogs(12345)
-        self.assertTrue(len(got_blogs) == 1)
--- tests/test_comment.py
+++ tests/test_comment.py
@@ -2,19 +2,15 @@ import unittest
 from app.models import Blog, User, Comment
 from app import db
 
-class TestBlog(unittest.TestCase):
+class CommentModelTest(unittest.TestCase):
 
     def setUp(self):
-        self.new_blog = Blog(description = "comment")
+        self.new_blog = Blog(title = "title", description = "comment")
         self.new_comment = Comment(description = "pineapples", blog=self.new_blog)
     
     def tearDown(self):
-        User.query.delete()
         Blog.query.delete()
-        Comment.query.delete()      
-
-    def test_instance(self):
-        self.assertTrue(isinstance(self.new_comment,Comment))
+        Comment.query.delete()
 
     def test_check_instance_variables(self):
         self.assertEquals(self.new_comment.description,"pineapples")
@@ -23,8 +19,3 @@ class TestBlog(unittest.TestCase):
     def test_save_comment(self):
         self.new_comment.save_comment()
         self.assertTrue(len(Comment.query.all())>0)
-
-    def test_get_comment_by_id(self):
-        self.new_comment.save_comment()
-        got_comments = Comment.get_comments(12345)
-        self.assertTrue(len(got_comments) == 1)
